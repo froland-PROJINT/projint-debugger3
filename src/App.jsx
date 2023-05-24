60cdf73b2871a178746180c799af8df6ce9a56cb
Francois Roland <froland@froland.be>
2023-05-24 18:14:41 +0200

    Create todo list app with a bug.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {useState} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [description, setDescription] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  function handleChange(event) {
+    setDescription(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newTask = {id: crypto.randomUUID(), description: description, done: false};
+    console.debug("add task " + newTask.id);
+    setTaskList([...taskList, newTask]);
+    setDescription('');
+  }
+
+  function handleDelete(taskId) {
+    console.debug("delete task " + taskId);
+    const newTaskList = taskList.filter((task) => task.id !== taskId);
+    setTaskList(newTaskList);
+  }
+
+  function toggle(taskId) {
+    console.debug("toggle task " + taskId);
+    const newTaskList = taskList.map((task) => {
+      if (task.id === task.id) {
+        return {...task, done: !task.done};
+      }
+      return task;
+    });
+    setTaskList(newTaskList);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <>
+        <h1>To Do</h1>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Ajouter une tâche</legend>
+            <label htmlFor="task">Description: </label>
+            <input type="text" id="task" value={description} onChange={handleChange}/>
+            <button type="submit">Ajouter</button>
+          </fieldset>
+        </form>
+        <ul id="todo-list">
+          {taskList.map((task) =>
+              <li key={task.id}>
+                <input type="checkbox" checked={task.done} onClick={() => toggle(task.id)} readOnly/>
+                <span className="description">{task.description}</span>
+                <a href="#" onClick={() => handleDelete(task.id)}>supprimer</a>
+              </li>
+          )}
+        </ul>
+      </>
+  );
 }
 
-export default App
+export default App;
